806154f4715adcfa1189f50fabb2bd41f970b1e4
Blair Fitzhugh <bfitz1@umbc.edu>
2024-02-27 16:16:44 -0500

    Complete step 95

--- javascript-algorithms-and-data-structures/role-playing-game/script.js
+++ javascript-algorithms-and-data-structures/role-playing-game/script.js
@@ -94,7 +94,7 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-    if (currentWeapon < 3) {
+    if (currentWeapon < weapons.length) {
         if (gold >= 30) {
             gold -= 30;
             currentWeapon++;
