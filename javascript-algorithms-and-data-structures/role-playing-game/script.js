403ff380940a95a6b2567ee268dc3606aea56f54
Blair Fitzhugh <bfitz1@umbc.edu>
2024-02-28 00:26:07 -0500

    Complete step 137

--- javascript-algorithms-and-data-structures/role-playing-game/script.js
+++ javascript-algorithms-and-data-structures/role-playing-game/script.js
@@ -178,7 +178,11 @@ function attack() {
     if (health <= 0) {
         lose();
     } else if (monsterHealth <= 0) {
-        defeatMonster();
+        if (fighting === 2) {
+            winGame();
+        } else {
+            defeatMonster();
+        }
     }
 }
 
