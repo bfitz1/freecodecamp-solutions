f94d7b55a8dbbf2c3e46b41995c88d20619c40c5
Blair Fitzhugh <bfitz1@umbc.edu>
2024-02-27 19:09:51 -0500

    Complete step 126

--- javascript-algorithms-and-data-structures/role-playing-game/script.js
+++ javascript-algorithms-and-data-structures/role-playing-game/script.js
@@ -170,7 +170,7 @@ function attack() {
 }
 
 function dodge() {
-
+    text.innerText = "You dodge the attack from the " + monsters[fighting].name;
 }
 
 function defeatMonster() {
@@ -178,5 +178,5 @@ function defeatMonster() {
 }
 
 function lose() {
-    
+
 }
