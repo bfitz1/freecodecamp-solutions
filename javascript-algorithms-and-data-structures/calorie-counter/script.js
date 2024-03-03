742d8888c53d69f043e3900614c0b10c43b1015d
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-03 15:43:29 -0500

    Complete step 91

--- javascript-algorithms-and-data-structures/calorie-counter/script.js
+++ javascript-algorithms-and-data-structures/calorie-counter/script.js
@@ -82,6 +82,7 @@ function clearForm() {
     for (const container of inputContainers) {
         container.innerHTML = "";
     }
+    budgetNumberInput.value = "";
 }
 
 addEntryButton.addEventListener("click", addEntry);
