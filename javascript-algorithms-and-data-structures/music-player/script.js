57b4c71ec0f79b4c76d8671c03a41ab5ea78112f
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-03 17:32:51 -0500

    Complete step 24

--- javascript-algorithms-and-data-structures/music-player/script.js
+++ javascript-algorithms-and-data-structures/music-player/script.js
@@ -107,6 +107,8 @@ renderSongs(userData?.songs);
 
 const sortSongs = () => {
     userData?.songs.sort((a, b) => {
-        
+        if (a.title < b.title) {
+            return -1;
+        }
     });
 };
