57fbc2f7ad8f466470389ad2066baa8b92b979bb
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-04 01:42:54 -0500

    Start step 90
    Swap out the playlist for shorter songs

--- javascript-algorithms-and-data-structures/music-player/script.js
+++ javascript-algorithms-and-data-structures/music-player/script.js
@@ -5,6 +5,7 @@ const nextButton = document.getElementById('next');
 const previousButton = document.getElementById('previous');
 const shuffleButton = document.getElementById('shuffle');
 
+/*
 const allSongs = [
     {
         id: 0,
@@ -75,6 +76,45 @@ const allSongs = [
         artist: "Quincy Larson",
         duration: "2:43",
         src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/chasing-that-feeling.mp3",
+    },
+];
+*/
+
+const allSongs = [
+    {
+        id: 0,
+        title: "Hello World",
+        artist: "Rafael",
+        duration: "0:23",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/hello-world.mp3",
+    },
+    {
+        id: 1,
+        title: "In the Zone",
+        artist: "Rafael",
+        duration: "0:11",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/in-the-zone.mp3",
+    },
+    {
+        id: 2,
+        title: "Camper Cat",
+        artist: "Rafael",
+        duration: "0:21",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/camper-cat.mp3",
+    },
+    {
+        id: 3,
+        title: "Electronic",
+        artist: "Rafael",
+        duration: "0:15",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/electronic.mp3",
+    },
+    {
+        id: 4,
+        title: "Sailing Away",
+        artist: "Rafael",
+        duration: "0:22",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/sailing-away.mp3",
     },
 ];
 
