26f9b91553c1a2328798af046f90c9aae05636ff
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-03 21:31:24 -0500

    Complete step 58

--- javascript-algorithms-and-data-structures/music-player/script.js
+++ javascript-algorithms-and-data-structures/music-player/script.js
@@ -129,7 +129,9 @@ const playPreviousSong = () => {
 const highlightCurrentSong = () => {
     const playlistSongElements = document.querySelectorAll('.playlist-song');
     const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
-    playlistSongElements.forEach((songEl) => {})
+    playlistSongElements.forEach((songEl) => {
+        songEl.removeAttribute('aria-current');
+    });
 }
 
 const renderSongs = (array) => {
