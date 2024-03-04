6bd9c0dca197d3e667de04b1b51f8fdbba5d332d
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-03 21:13:25 -0500

    Complete step 54

--- javascript-algorithms-and-data-structures/music-player/script.js
+++ javascript-algorithms-and-data-structures/music-player/script.js
@@ -157,6 +157,7 @@ playButton.addEventListener('click', () => {
 
 pauseButton.addEventListener('click', pauseSong);
 nextButton.addEventListener('click', playNextSong);
+previousButton.addEventListener('click', playPreviousSong);
 
 const sortSongs = () => {
     userData?.songs.sort((a, b) => {
