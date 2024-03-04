79b25f03ba3c3da250d7b66245ed6387270e4a44
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-03 19:41:34 -0500

    Complete step 38

--- javascript-algorithms-and-data-structures/music-player/script.js
+++ javascript-algorithms-and-data-structures/music-player/script.js
@@ -120,6 +120,8 @@ const renderSongs = (array) => {
 playButton.addEventListener('click', () => {
     if (!userData?.currentSong) {
         playSong(userData?.songs[0].id);
+    } else {
+        playSong(userData?.currentSong.id);
     }
 });
 renderSongs(sortSongs());
