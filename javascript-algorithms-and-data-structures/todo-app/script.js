c16509d26a9b00acf841ca61bb3ee7900f18a27e
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-06 01:35:05 -0500

    Complete step 9

--- javascript-algorithms-and-data-structures/todo-app/script.js
+++ javascript-algorithms-and-data-structures/todo-app/script.js
@@ -22,3 +22,7 @@ closeTaskFormBtn.addEventListener('click', () => {
 cancelBtn.addEventListener('click', () => {
     confirmCloseDialog.close();
 })
+discardBtn.addEventListener('click', () => {
+    confirmCloseDialog.close();
+    taskForm.classList.toggle('hidden');
+})
