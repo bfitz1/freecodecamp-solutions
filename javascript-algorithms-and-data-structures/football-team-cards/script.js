d9bb5de2e665bf5d59f658a34041ec91cb9c9c59
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-05 23:11:07 -0500

    Complete step 20

--- javascript-algorithms-and-data-structures/football-team-cards/script.js
+++ javascript-algorithms-and-data-structures/football-team-cards/script.js
@@ -175,3 +175,5 @@ Object.freeze(myFavoriteFootballTeam);
 
 const { sport, team, year, players } = myFavoriteFootballTeam;
 const { coachName } = myFavoriteFootballTeam.headCoach;
+
+typeOfSport.textContent = sport;
