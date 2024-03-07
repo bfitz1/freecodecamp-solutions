eb5ea0282dfdaca6d7acd1d65ec675b15fd00c99
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-07 02:32:47 -0500

    Complete step 80

--- javascript-algorithms-and-data-structures/decimal-binary-converter/script.js
+++ javascript-algorithms-and-data-structures/decimal-binary-converter/script.js
@@ -12,7 +12,7 @@ const decimalToBinary = (input) => {
 
 const showAnimation = () => {
     console.log('free');
-    console.log('Code');
+    setTimeout(() => { console.log('Code'); }, 1000);
     console.log('Camp');
 };
 
