dd9b887d15fe9991c360057d8ba1ebc90280ffea
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-07 20:34:13 -0500

    Complete step 39

--- javascript-algorithms-and-data-structures/number-sorter/script.js
+++ javascript-algorithms-and-data-structures/number-sorter/script.js
@@ -4,7 +4,7 @@ const sortInputArray = (event) => {
     event.preventDefault();
 
     const inputValues = [...document.getElementsByClassName('values-dropdown')].map((dropdown) => Number(dropdown.value));
-    const sortedValues = insertionSort(inputValues);
+    const sortedValues = inputValues.sort();
     updateUI(sortedValues);
 };
 
