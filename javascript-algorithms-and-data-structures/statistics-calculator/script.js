25bf4dded728ce07fc981e493d1caff872340b04
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-08 12:22:01 -0500

    Complete step 28

--- javascript-algorithms-and-data-structures/statistics-calculator/script.js
+++ javascript-algorithms-and-data-structures/statistics-calculator/script.js
@@ -11,6 +11,9 @@ const getMedian = (array) => {
 const getMode = (array) => {
     const counts = {};
     array.forEach((el) => { counts[el] = (counts[el] || 0) + 1 });
+    if (new Set(Object.values(counts)).size === 1) {
+        return null;
+    }
 };
 
 const calculate = () => {
