2f465140afd39674e86c47c30e0065fc695c684a
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-08 12:36:28 -0500

    Complete step 39

--- javascript-algorithms-and-data-structures/statistics-calculator/script.js
+++ javascript-algorithms-and-data-structures/statistics-calculator/script.js
@@ -24,6 +24,7 @@ const getRange = (array) => Math.max(...array) - Math.min(...array);
 const getVariance = (array) => {
     const mean = getMean(array);
     const differences = array.map((el) => el - mean);
+    const squaredDifferences = differences.map((el) => el ** 2);
 }
 
 const calculate = () => {
