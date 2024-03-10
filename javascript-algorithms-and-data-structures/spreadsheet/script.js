157fc698aae676e4b8f84b83751429714ca64194
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-10 13:40:21 -0400

    Complete step 33

--- javascript-algorithms-and-data-structures/spreadsheet/script.js
+++ javascript-algorithms-and-data-structures/spreadsheet/script.js
@@ -17,6 +17,8 @@ const spreadsheetFunctions = {
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map((code) => String.fromCharCode(code));
 
+const evalFormula = (x, cells) => {};
+
 window.onload = () => {
     const container = document.getElementById('container');
     const createLabel = (name) => {
