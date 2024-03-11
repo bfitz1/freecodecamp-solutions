410ac75862b7e6e29932f7bc794fe34f98059c82
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-10 23:58:46 -0400

    Complete step 72

--- javascript-algorithms-and-data-structures/spreadsheet/script.js
+++ javascript-algorithms-and-data-structures/spreadsheet/script.js
@@ -7,7 +7,7 @@ const infixToFunction = {
 
 const infixEval = (str, regex) => str.replace(
     regex, 
-    (_match, arg1, operator, arg2) => infixToFunction[operator](arg1, arg2)
+    (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1), parseFloat(arg2))
 );
 
 const sum = (nums) => nums.reduce((acc, el) => acc + el);
