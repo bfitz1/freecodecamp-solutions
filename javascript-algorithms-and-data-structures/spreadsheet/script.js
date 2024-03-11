7881b56981014a2d928ceb3031a1fe0025d7cbd8
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-11 01:21:13 -0400

    Complete step 99

--- javascript-algorithms-and-data-structures/spreadsheet/script.js
+++ javascript-algorithms-and-data-structures/spreadsheet/script.js
@@ -33,11 +33,12 @@ const spreadsheetFunctions = {
     average, 
     median,
     even: (nums) => nums.filter(isEven),
+    someeven: (nums) => nums.some(isEven),
+    everyeven: (nums) => nums.every(isEven),
     firsttwo: (nums) => nums.slice(0, 2),
     lasttwo: (nums) => nums.slice(nums.length - 2),
     has2: (nums) => nums.includes(2),
     increment: (nums) => nums.map((el) => el + 1),
-    someeven: (nums) => nums.some(isEven),
 };
 
 const applyFunction = (str) => {
