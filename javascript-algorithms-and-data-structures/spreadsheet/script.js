514edf2db6ed179d77092bc30beea11b776d93f0
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-11 00:01:08 -0400

    Complete step 73

--- javascript-algorithms-and-data-structures/spreadsheet/script.js
+++ javascript-algorithms-and-data-structures/spreadsheet/script.js
@@ -10,6 +10,8 @@ const infixEval = (str, regex) => str.replace(
     (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1), parseFloat(arg2))
 );
 
+const highPrecedence = (str) => {};
+
 const sum = (nums) => nums.reduce((acc, el) => acc + el);
 const isEven = (num) => num % 2 === 0;
 const average = (nums) => sum(nums) / nums.length;
