1c30e7579ae58f88a2b4a5e4164240dc8c8c7a0e
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-14 18:11:51 -0400

    Complete step 39

--- javascript-algorithms-and-data-structures/shopping-cart/script.js
+++ javascript-algorithms-and-data-structures/shopping-cart/script.js
@@ -138,4 +138,5 @@ const addToCartBtns = document.getElementsByClassName('add-to-cart-btn');
 
 cartBtn.addEventListener('click', () => {
     isCartShowing = !isCartShowing;
+    showHideCartSpan.textContent = isCartShowing ? 'Hide' : 'Show';
 });
