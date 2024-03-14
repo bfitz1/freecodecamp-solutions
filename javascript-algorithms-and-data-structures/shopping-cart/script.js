6aea1b18dbc85f1ff8ffcb5d7787b6820ad99b06
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-14 19:00:16 -0400

    Complete step 57

--- javascript-algorithms-and-data-structures/shopping-cart/script.js
+++ javascript-algorithms-and-data-structures/shopping-cart/script.js
@@ -137,6 +137,10 @@ class ShoppingCart {
         }
 
         const isCartCleared = confirm('Are you sure you want to clear all items from your shopping cart?');
+        if (isCartCleared) {
+            this.items = [];
+            this.total = 0;
+        }
     }
 
     calculateTaxes(amount) {
