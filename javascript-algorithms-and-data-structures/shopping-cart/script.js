b78c07f3568882267c3e67dcd02ec8f1a6d5b9fb
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-14 16:02:04 -0400

    Complete step 28

--- javascript-algorithms-and-data-structures/shopping-cart/script.js
+++ javascript-algorithms-and-data-structures/shopping-cart/script.js
@@ -116,6 +116,6 @@ class ShoppingCart {
         const currentProductCount = totalCountPerProduct[product.id];
         const currentProductCountSpan = document.getElementById(`product-count-for-id${product.id}`);
 
-        currentProductCount > 1 ? undefined : undefined;
+        currentProductCount > 1 ? currentProductCountSpan.textContent = `${currentProductCount}x` : undefined;
     }
 };
