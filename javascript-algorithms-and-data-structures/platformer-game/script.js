91812abe371a3c943226e5dceb71a77d4b11c2e5
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-16 13:41:32 -0400

    Complete step 56

--- javascript-algorithms-and-data-structures/platformer-game/script.js
+++ javascript-algorithms-and-data-structures/platformer-game/script.js
@@ -89,6 +89,8 @@ const movePlayer = (key, xVelocity, isPressed) => {
             if (xVelocity === 0) {
                 player.velocity.x = xVelocity;
             }
+            player.velocity.x -= xVelocity;
+            break;
     }
 };
 
