b78803fb4f3d47f34a1534c0bba501f76b9b21c0
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-19 19:16:37 -0400

    Complete step 4

--- javascript-algorithms-and-data-structures/dice-game/script.js
+++ javascript-algorithms-and-data-structures/dice-game/script.js
@@ -3,3 +3,5 @@ const scoreInputs = document.querySelectorAll('#score-options input');
 const scoreSpans = document.querySelectorAll('#score-options span');
 const currentRoundText = document.getElementById('current-round');
 const currentRoundRollsText = document.getElementById('current-round-rolls');
+const totalScoreText = document.getElementById('total-score');
+const scoreHistory = document.getElementById('score-history');
