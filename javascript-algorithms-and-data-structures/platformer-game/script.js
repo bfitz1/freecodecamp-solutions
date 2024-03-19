9d1393d980493e7814ad415fbe26cfa11484d4eb
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-19 15:43:32 -0400

    Complete step 107

--- javascript-algorithms-and-data-structures/platformer-game/script.js
+++ javascript-algorithms-and-data-structures/platformer-game/script.js
@@ -224,6 +224,7 @@ const startGame = () => {
 
 const showCheckpointScreen = (msg) => {
     checkpointScreen.style.display = 'block';
+    checkpointMessage.textContent = msg;
 };
 
 startBtn.addEventListener('click', startGame);
