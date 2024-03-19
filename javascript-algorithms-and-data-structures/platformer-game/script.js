c43de3e31258927191054e55fb8de0a00d6309a7
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-19 13:37:08 -0400

    Complete step 71

--- javascript-algorithms-and-data-structures/platformer-game/script.js
+++ javascript-algorithms-and-data-structures/platformer-game/script.js
@@ -71,6 +71,8 @@ class Platform {
 
 const player = new Player();
 
+const platformPositions = [];
+
 const animate = () => {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
