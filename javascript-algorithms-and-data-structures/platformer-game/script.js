2718594ffb72fc27185e9d027aaf1cb829cafc4d
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-19 15:10:25 -0400

    Complete step 98

--- javascript-algorithms-and-data-structures/platformer-game/script.js
+++ javascript-algorithms-and-data-structures/platformer-game/script.js
@@ -70,11 +70,16 @@ class Platform {
 };
 
 class CheckPoint {
-    constructor (x, y, z) {
+    constructor(x, y, z) {
         this.position = { x, y };
         this.width = proportionalSize(40);
         this.height = proportionalSize(70);
         this.claimed = false;
+    }
+
+    draw() {
+        ctx.fillStyle = '#f1be32';
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 };
 
