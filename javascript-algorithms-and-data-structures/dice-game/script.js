39413d06930a5f1407c1f8603d2fa5279f60139d
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-19 23:17:55 -0400

    Complete step 29

--- javascript-algorithms-and-data-structures/dice-game/script.js
+++ javascript-algorithms-and-data-structures/dice-game/script.js
@@ -35,6 +35,7 @@ const updateStats = () => {
 
 const updateRadioOption = (optionNode, score) => {
     scoreInputs[optionNode].disabled = false;
+    scoreInputs[optionNode].value = score;
 };
 
 rollDiceBtn.addEventListener('click', () => {
