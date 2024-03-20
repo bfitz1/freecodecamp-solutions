133ba9fe64a6a6e854d7f2befaf3b9e57170e711
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-20 11:40:34 -0400

    Complete step 85

--- javascript-algorithms-and-data-structures/dice-game/script.js
+++ javascript-algorithms-and-data-structures/dice-game/script.js
@@ -103,6 +103,10 @@ const checkForStraights = (arr) => {
     if (smallStraightsArr.includes(uniqueNumbersStr)) {
         updateRadioOption(3, 30);
     }
+    
+    if (largeStraightsArr.includes(uniqueNumbersStr)) {
+        updateRadioOption(4, 40);
+    }
 };
 
 const resetRadioOption = () => {
