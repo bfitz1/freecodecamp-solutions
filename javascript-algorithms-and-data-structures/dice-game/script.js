d2f726de6cca252a19c2861d57f884bb67f5c28c
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-19 20:48:59 -0400

    Complete step 23

--- javascript-algorithms-and-data-structures/dice-game/script.js
+++ javascript-algorithms-and-data-structures/dice-game/script.js
@@ -28,6 +28,8 @@ const rollDice = () => {
     });
 };
 
+const updateStats = () => {};
+
 rollDiceBtn.addEventListener('click', () => {
     if (rolls === 3) {
         alert('You have made three rolls this round. Please select a score.');
