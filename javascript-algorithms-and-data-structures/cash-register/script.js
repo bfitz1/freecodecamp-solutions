c1032c445486dd04ac7e8fb1333994260e4723fe
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-21 02:45:57 -0400

    Pass the tests

--- javascript-algorithms-and-data-structures/cash-register/script.js
+++ javascript-algorithms-and-data-structures/cash-register/script.js
@@ -1,6 +1,5 @@
-// Given by fCC; just remember these are declared using `let` so fCC's
-// tests work.
-let price = 1.87;
+// Just remember that `price` and `cid` should be declared using `let`.
+let price = 19.5;
 let cid = [
   ["PENNY", 1.01],
   ["NICKEL", 2.05],
@@ -13,14 +12,13 @@ let cid = [
   ["ONE HUNDRED", 100]
 ];
 
-// DOM elements
+// DOM elements.
 const change_due = document.getElementById('change-due');
 const cash_input = document.getElementById('cash');
 const purchase_btn = document.getElementById('purchase-btn');
 
-// Sucks that I got saddled with using floating-point numbers instead
-// of 
-const currency_ref = {
+// The choice of floating-point values is a bit unfortunate.
+const denominations = {
     'PENNY': 0.01,
     'NICKEL': 0.05,
     'DIME': 0.1,
@@ -32,46 +30,51 @@ const currency_ref = {
     'ONE HUNDRED': 100,
 };
 
-// Maybe this is too dense?
-const calculate_change = (cash) => cid.reduceRight(([remaining, drawer, change], [name, amount]) => {
-    const unit = currency_ref[name];
-    const cash_unit = parseFloat((Math.floor(remaining / unit) * unit).toFixed(2));
-    if (remaining >= unit && amount > 0) {
-        const x = Math.min(cash_unit, amount);
+// Just because I'm using this everywhere.
+const toMoney = (x) => parseFloat(x.toFixed(2));
+
+const calculate_change = (cash) => cid.reduceRight(([remaining, drawer, changelist], [name, amount]) => {
+    const denom = denominations[name];
+    if (remaining >= denom && amount > 0) {
+        let change = toMoney(Math.floor(remaining / denom) * denom);
+        change = Math.min(change, amount);
         return [
-            parseFloat((remaining - x).toFixed(2)),
-            [[name, parseFloat((amount - x).toFixed(2))]].concat(drawer),
-            change.concat([[name, x]])
+            toMoney(remaining - change),
+            [[name, toMoney(amount - change)]].concat(drawer),
+            changelist.concat([[name, change]])
         ];
     } else {
-        return [remaining, [[name, amount]].concat(drawer), change];
+        return [remaining, [[name, amount]].concat(drawer), changelist];
     }
-}, [cash, cid, []]);
+}, [cash, [], []]);
 
 purchase_btn.addEventListener('click', () => {
     const cash = parseFloat(cash_input.value);
+
     if (cash < price) {
         alert('Customer does not have enough money to purchase the item');
+        return;
     } else if (cash === price) {
-        change_due.textContent = 'No change due - customer paid with exact cash';
-    } else {
-        const [remaining, drawer, change] = calculate_change(parseFloat((cash - price).toFixed(2)));
-        const funds = drawer.reduce((acc, [name, amount]) => acc + amount, 0);
+        change_due.innerHTML = '<p>No change due - customer paid with exact cash</p>';
+        return;
+    }
 
-        if (remaining > 0) {
-            change_due.innerHTML = '<p>Status: INSUFFICIENT_FUNDS</p>';
-        } else if (remaining === 0 && funds === 0) {
-            change_due.innerHTML = '<p>Status: CLOSED</p>'
-            change_due.innerHTML += change.map(([name, amount]) => {
-                return `<p>${name}: $${amount}</p>`
-            }).join('');
-        } else if (remaining === 0 && funds > 0) {
-            change_due.innerHTML = '<p>Status: OPEN</p>'
-            change_due.innerHTML += change.map(([name, amount]) => {
-                return `<p>${name}: $${amount}</p>`
-            }).join('');
-        } else {
-            console.error(`Something weird happened: Remaining: ${remaining}, funds: ${funds}`);
-        }
+    const [remaining, drawer, change] = calculate_change(toMoney(cash - price));
+    const funds = drawer.reduce((acc, [name, amount]) => acc + amount, 0);
+
+    if (remaining > 0) {
+        change_due.innerHTML = '<p>Status: INSUFFICIENT_FUNDS</p>';
+    } else if (remaining === 0 && funds === 0) {
+        change_due.innerHTML = '<p>Status: CLOSED</p>'
+        change_due.innerHTML += change.map(([name, amount]) => {
+            return `<p>${name}: $${amount}</p>`
+        }).join('');
+    } else if (remaining === 0 && funds > 0) {
+        change_due.innerHTML = '<p>Status: OPEN</p>'
+        change_due.innerHTML += change.map(([name, amount]) => {
+            return `<p>${name}: $${amount}</p>`
+        }).join('');
+    } else {
+        console.error(`Something weird happened: Remaining: ${remaining}, funds: ${funds}`);
     }
 });
