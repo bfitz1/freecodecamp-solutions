55987d1542c5ad41108cfc3b55727335d987c941
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-22 14:57:57 -0400

    Complete step 17

--- javascript-algorithms-and-data-structures/forum-leaderboard/script.js
+++ javascript-algorithms-and-data-structures/forum-leaderboard/script.js
@@ -24,5 +24,5 @@ const showLatestPosts = (data) => {
     postsContainer.innerHTML = topics.map((item) => {
         const {id, title, views, posts_count, slug, posters, category_id, bumped_at} = item;
         return `<tr></tr>`;
-    });
+    }).join('');
 };
