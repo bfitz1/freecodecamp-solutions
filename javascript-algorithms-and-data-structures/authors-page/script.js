e9b3a84ac66124c4af20768ed3a632390c3eb89a
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-22 02:43:37 -0400

    Complete step 8

--- javascript-algorithms-and-data-structures/authors-page/script.js
+++ javascript-algorithms-and-data-structures/authors-page/script.js
@@ -13,3 +13,5 @@ fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
     .catch((err) => {
         console.error(`There was an error: ${err}`);
     });
+
+const displayAuthors = (authors) => {};
