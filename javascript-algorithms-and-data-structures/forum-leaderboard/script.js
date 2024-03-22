c797ef8a5d96039f20d2aee52f5fb6f5f34e92fa
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-22 14:39:23 -0400

    Complete step 5

--- javascript-algorithms-and-data-structures/forum-leaderboard/script.js
+++ javascript-algorithms-and-data-structures/forum-leaderboard/script.js
@@ -5,4 +5,10 @@ const avatarUrl = 'https://sea1.discourse-cdn.com/freecodecamp';
 
 const postsContainer = document.getElementById('posts-container');
 
-const fetchData = async () => {};
+const fetchData = async () => {
+    try {
+
+    } catch (err) {
+        
+    }
+};
