5379b5c3bd794035ed5ee2d3bc74f27be6427fff
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-23 23:50:13 -0400

    Complete step 53

--- javascript-algorithms-and-data-structures/forum-leaderboard/script.js
+++ javascript-algorithms-and-data-structures/forum-leaderboard/script.js
@@ -56,6 +56,8 @@ const forumCategory = (id) => {
     const url = `${forumCategoryUrl}${selectedCategory.className}/${id}`;
     const linkText = selectedCategory.category;
     const linkClass = `category ${selectedCategory.className}`;
+
+    return `<a href="${url}"></a>`
 };
 
 const timeAgo = (time) => {
