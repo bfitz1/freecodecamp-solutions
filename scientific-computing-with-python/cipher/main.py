3a9ac6945f73063d401cb6cb912808c23cf17c7a
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-25 14:24:34 -0400

    Complete step 48

--- scientific-computing-with-python/cipher/main.py
+++ scientific-computing-with-python/cipher/main.py
@@ -11,4 +11,4 @@ for char in text.lower():
         new_index = (index + shift) % len(alphabet)
         encrypted_text += alphabet[new_index]
     
-    print('char:', char, 'encrypted text:', encrypted_text)
+print('encrypted text:', encrypted_text)
