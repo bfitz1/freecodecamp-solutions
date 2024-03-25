461a501ce826b103d34a58c1ba0a366e3b70a60f
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-25 14:47:01 -0400

    Complete step 66

--- scientific-computing-with-python/cipher/main.py
+++ scientific-computing-with-python/cipher/main.py
@@ -20,5 +20,4 @@ def vigenere(message, key):
             new_index = (index + offset) % len(alphabet)
             encrypted_text += alphabet[new_index]
         
-    print('plain text:', message)
-    print('encrypted text:', encrypted_text)
+    return encrypted_text
