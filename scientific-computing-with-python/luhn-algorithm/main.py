90ddeaeebb07ba0a23e47d5385d084b0e15b5ba0
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-26 00:03:38 -0400

    Complete step 17

--- scientific-computing-with-python/luhn-algorithm/main.py
+++ scientific-computing-with-python/luhn-algorithm/main.py
@@ -1,6 +1,7 @@
 def verify_card_number(card_number):
     sum_of_odd_digits = 0
     card_number_reversed = card_number[::-1]
+    odd_digits = card_number_reversed[1::2]
 
 def main():
     card_number = '4111-1111-4555-1142'
