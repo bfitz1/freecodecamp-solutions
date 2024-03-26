b1fe7a645b00eca228325d966d261b30a52140ae
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-26 00:09:03 -0400

    Complete step 23

--- scientific-computing-with-python/luhn-algorithm/main.py
+++ scientific-computing-with-python/luhn-algorithm/main.py
@@ -1,14 +1,14 @@
 def verify_card_number(card_number):
     sum_of_odd_digits = 0
     card_number_reversed = card_number[::-1]
-    odd_digits = card_number_reversed[1::2]
+    odd_digits = card_number_reversed[::2]
 
     for digit in odd_digits:
         sum_of_odd_digits += int(digit)
-    
     print(sum_of_odd_digits)
 
     sum_of_even_digits = 0
+    even_digits = card_number_reversed[1::2]
 
 def main():
     card_number = '4111-1111-4555-1142'
