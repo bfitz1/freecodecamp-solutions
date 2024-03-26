9b20421d834754c9e66545b86f008ebe301ffafe
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-26 01:27:15 -0400

    Complete step 16

--- scientific-computing-with-python/expense-tracker/main.py
+++ scientific-computing-with-python/expense-tracker/main.py
@@ -6,6 +6,6 @@ def print_expenses(expenses):
         print(f"Amount: {expense['amount']}, Category: {expense['category']}")
 
 def total_expenses(expenses):
-    pass
+    lambda expense: expense['amount']
 
 expenses = []
