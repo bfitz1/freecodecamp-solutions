87087020ef820b80f29a20a96580bc7009afc5be
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-27 14:34:46 -0400

    Complete step 46

--- scientific-computing-with-python/tower-of-hanoi/main.py
+++ scientific-computing-with-python/tower-of-hanoi/main.py
@@ -6,6 +6,7 @@ def move(n, source, auxiliary, target):
         # move n - 1 disks from source to auxiliary, so they are out of the way
         move(n - 1, source, auxiliary, target)
         # display starting configuration
+        rods[target].append(rods[source].pop())
         print(rods, '\n')
 
 # initiate call from source A to target C with auxiliary B
