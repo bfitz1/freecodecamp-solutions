be4ca78a4b0c2f33204d0181d5e70345cedf0a9d
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-27 00:48:18 -0400

    Complete step 33

--- scientific-computing-with-python/shortest-path/main.py
+++ scientific-computing-with-python/shortest-path/main.py
@@ -12,4 +12,4 @@ def shortest_path(graph, start):
     paths[start].append(start)
     print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
 
-shortest_path(my_graph, 'A')
+#shortest_path(my_graph, 'A')
