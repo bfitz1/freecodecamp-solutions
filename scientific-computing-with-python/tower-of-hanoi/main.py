15e7d5383f47a3069c08bb78fb608aea0da43366
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-27 14:31:04 -0400

    Complete step 45

--- scientific-computing-with-python/tower-of-hanoi/main.py
+++ scientific-computing-with-python/tower-of-hanoi/main.py
@@ -3,6 +3,7 @@ rods = {'A': list(range(NUMBER_OF_DISKS, 0, -1)), 'B': [], 'C': []}
 
 def move(n, source, auxiliary, target):
     if n > 0:
+        # move n - 1 disks from source to auxiliary, so they are out of the way
         move(n - 1, source, auxiliary, target)
         # display starting configuration
         print(rods, '\n')
