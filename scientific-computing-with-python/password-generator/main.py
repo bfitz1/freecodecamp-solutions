8a1290dbedd2a1037252244208d501ac1a65ac0e
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-26 22:25:49 -0400

    Complete step 65

--- scientific-computing-with-python/password-generator/main.py
+++ scientific-computing-with-python/password-generator/main.py
@@ -31,5 +31,5 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
     
     return password
 
-#new_password = generate_password(8)
-#print(new_password)
+new_password = generate_password(8, 1, 1, 1, 1)
+print(new_password)
