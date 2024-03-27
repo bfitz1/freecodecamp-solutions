8218a0e80823618be95c6ec68259c395be7fecf8
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-27 14:52:05 -0400

    Complete step 52

--- scientific-computing-with-python/tower-of-hanoi/main.py
+++ scientific-computing-with-python/tower-of-hanoi/main.py
@@ -1,16 +1,18 @@
 NUMBER_OF_DISKS = 4
-rods = {'A': list(range(NUMBER_OF_DISKS, 0, -1)), 'B': [], 'C': []}
+A = list(range(NUMBER_OF_DISKS, 0, -1))
+B = []
+C = []
 
 def move(n, source, auxiliary, target):
     if n > 0:
         # move n - 1 disks from source to auxiliary, so they are out of the way
         move(n - 1, source, target, auxiliary)
         # move the nth disk from source to target
-        rods[target].append(rods[source].pop())
+        target.append(source.pop())
         # display our progress
-        print(rods, '\n')
+        print(A, B, C, '\n')
         # move the n - 1 disks that we left on auxiliary onto target
         move(n - 1, auxiliary, source, target)
 
 # initiate call from source A to target C with auxiliary B
-move(NUMBER_OF_DISKS, 'A', 'B', 'C')
+move(NUMBER_OF_DISKS, A, B, C)
