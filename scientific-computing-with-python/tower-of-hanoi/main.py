30561af7e1eceed0b92310a53f5445d2050c0b9d
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-27 14:30:23 -0400

    Complete step 44

--- scientific-computing-with-python/tower-of-hanoi/main.py
+++ scientific-computing-with-python/tower-of-hanoi/main.py
@@ -2,8 +2,10 @@ NUMBER_OF_DISKS = 4
 rods = {'A': list(range(NUMBER_OF_DISKS, 0, -1)), 'B': [], 'C': []}
 
 def move(n, source, auxiliary, target):
-    # display starting configuration
-    print(rods, '\n')
+    if n > 0:
+        move(n - 1, source, auxiliary, target)
+        # display starting configuration
+        print(rods, '\n')
 
 # initiate call from source A to target C with auxiliary B
 move(NUMBER_OF_DISKS, 'A', 'B', 'C')
