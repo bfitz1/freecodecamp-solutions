d386ae177b64c5570c71904f32af01f197bb31f2
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-27 14:10:35 -0400

    Complete step 39

--- scientific-computing-with-python/tower-of-hanoi/main.py
+++ scientific-computing-with-python/tower-of-hanoi/main.py
@@ -27,6 +27,9 @@ def move(n, source, auxiliary, target):
             if n % 2 == 1:
                 print(f'Move {i + 1} allowed between {source} and {target}')
                 make_allowed_move(source, target)
+            else:
+                print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+                #make_allowed_move(source, auxiliary)                
         elif remainder == 2:
             print(f'Move {i + 1} allowed between {source} and {auxiliary}')
             make_allowed_move(source, auxiliary)
