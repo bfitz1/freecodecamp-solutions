cbbee22f1a8990b06904ab936246ea2a2217d61d
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-28 01:36:49 -0400

    Complete step 16

--- scientific-computing-with-python/sudoku-solver/main.py
+++ scientific-computing-with-python/sudoku-solver/main.py
@@ -11,3 +11,4 @@ class Board:
             row_list = []
             for square_no, part in enumerate([line[:3], line[3:6], line[6:]], start=1):
                 row_square = '|'.join(str(item) for item in part)
+                row_list.extend(row_square)
