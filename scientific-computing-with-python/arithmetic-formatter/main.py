c94db937c29a975ef013365fbc4bde2920b1a239
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-29 01:38:49 -0400

    Write out validation functions

--- scientific-computing-with-python/arithmetic-formatter/main.py
+++ scientific-computing-with-python/arithmetic-formatter/main.py
@@ -1,3 +1,17 @@
+import string
+
+def is_valid_size(problems):
+    return len(problems) <= 5
+
+def is_valid_operator(op):
+    return op == '+' or op == '-'
+
+def is_valid_operand(op):
+    return all(ch in string.digits for ch in op)
+
+def is_valid_width(op):
+    return len(op) <= 4
+
 def arithmetic_arranger(problems, show_answers=False):
     return problems
 
