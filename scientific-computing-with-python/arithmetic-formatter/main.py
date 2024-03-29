e31dd1dc9edf29935c37a7f5b96d15b6edf59306
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-29 14:43:29 -0400

    Some refactoring

--- scientific-computing-with-python/arithmetic-formatter/main.py
+++ scientific-computing-with-python/arithmetic-formatter/main.py
@@ -1,28 +1,26 @@
-import string
-
 def is_valid_size(problems):
     return len(problems) <= 5
 
 def is_valid_operator(op):
-    return op == '+' or op == '-'
+    return op in '+-'
 
 def is_valid_operand(op):
-    return all(ch in string.digits for ch in op)
+    return all(ch.isdigit() for ch in op)
 
 def is_valid_width(op):
     return len(op) <= 4
 
-def format_operand1(op, width):
-    return (' ' * (width - len(op))) + op
+def format_term1(op, width):
+    return op.rjust(width)
 
-def format_operand2(op, pom, width):
-    return pom + (' ' * (width - len(op) - 1)) + op
+def format_term2(op, pom, width):
+    return op.rjust(width).replace(' ', pom, 1)
 
-def format_divider(width):
-    return '-' * width
+def format_dash(width):
+    return ''.rjust(width, '-')
 
 def format_solution(sol, width):
-    return ' ' * (width - len(sol)) + sol
+    return sol.rjust(width)
 
 def arithmetic_arranger(problems, show_answers=False):
     if not is_valid_size(problems):
@@ -37,27 +35,26 @@ def arithmetic_arranger(problems, show_answers=False):
     if not all(is_valid_width(lhs) and is_valid_width(rhs) for lhs, _, rhs in split_problems):
         return "Error: Numbers cannot be more than four digits."
 
-    operand1_line = ''
-    operand2_line = ''
-    dividing_line = ''
-    solution_line = ''
-
+    term1_line = []
+    term2_line = []
+    dash_line = []
+    solution_line = []
     for lhs, op, rhs in split_problems:
         width = max(len(lhs), len(rhs)) + 2
-        operand1_line += format_operand1(lhs, width) + ' ' * 4
-        operand2_line += format_operand2(rhs, op, width) + ' ' * 4
-        dividing_line += format_divider(width) + ' ' * 4
+        term1_line.append(format_term1(lhs, width))
+        term2_line.append(format_term2(rhs, op, width))
+        dash_line.append(format_dash(width))
         if show_answers:
             x = int(lhs)
             y = int(rhs)
             answer = x + y if op == '+' else x - y
-            solution_line += format_solution(str(answer), width) + ' ' * 4
+            solution_line.append(format_solution(str(answer), width))
 
     return '\n'.join([
-        operand1_line.rstrip(),
-        operand2_line.rstrip(),
-        dividing_line.rstrip(),
-        solution_line.rstrip(),
+        ''.rjust(4).join(term1_line),
+        ''.rjust(4).join(term2_line),
+        ''.rjust(4).join(dash_line),
+        ''.rjust(4).join(solution_line),
     ]).rstrip()
 
 print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], show_answers=True)}')
