a273005ec217350d7e04019bb9a107bac6e44b06
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-31 21:21:29 -0400

    Start writing skeleton code

--- scientific-computing-with-python/time-calculator/main.py
+++ scientific-computing-with-python/time-calculator/main.py
@@ -1,7 +1,16 @@
-def add_time(start, duration):
+import re
 
+# Since we may need to know from which day to offset
+WEEKDAYS = ('MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY')
 
+def parse_time(time):
+    pass
 
+def parse_duration(duration):
+    pass
 
-
-    return new_time
+# Considering an internal data representation like
+#   Time : Number [0, 2400)
+#   Day  : Number [0, 7)
+def add_time(start, duration, starting_day=None):
+    pass
