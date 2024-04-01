b1cace20499126c75eb44c3447e8703399f35e5c
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-31 22:47:59 -0400

    Sketch out category class

--- scientific-computing-with-python/budget-app/main.py
+++ scientific-computing-with-python/budget-app/main.py
@@ -1,5 +1,25 @@
 class Category:
-    pass
+    def __init__(self):
+        self.ledger = []
+    
+    def deposit(self, amount, description=None):
+        pass
+    
+    def withdraw(self, amount, description=None):
+        pass
+    
+    def get_balance(self):
+        pass
+    
+    def transfer(self, amount, category):
+        pass
+    
+    def check_funds(self, amount):
+        pass
+    
+    def __str__(self):
+        pass
+
 
 def create_spend_chart(categories):
     pass
