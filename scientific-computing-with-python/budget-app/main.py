9e82f43852ff815036d2ee5529ad9a2255d7eb8d
Blair Fitzhugh <bfitz1@umbc.edu>
2024-04-01 01:21:38 -0400

    Sigh, debugging

--- scientific-computing-with-python/budget-app/main.py
+++ scientific-computing-with-python/budget-app/main.py
@@ -33,22 +33,56 @@ class Category:
         total = 0
         for item in self.ledger:
             line = item['description'][:23].ljust(23, ' ')
-            line += str(item['amount']).rjust(7, ' ')
+            line += f"{item['amount']:.2f}".rjust(7, ' ')
             total += item['amount']
             lines.append(line)
         lines.append(f'Total: {total:.2f}')
         return '\n'.join(lines)
 
+def sum_category(category):
+    return sum(x['amount'] for x in category.ledger if x['amount'] < 0)
 
 def create_spend_chart(categories):
-    pass
+    # Sum withdrawals, grouping by category
+    groups = {
+        x.category : -sum(y['amount'] for y in x.ledger if y['amount'] < 0)
+        for x in categories
+    }
+    total = sum(groups.values())
+
+    # Tedious text formatting: first we format the data
+    width = 3*len(groups) + 1
+    print('Percentage spent by category')
+    for percentage in range(100, -1, -10):
+        print(f'{percentage:3}|', end='')
+        for spent in groups.values():
+            if round((spent / total) * 100) >= percentage:
+                print('o'.center(3), end='')
+            else:
+                print(' '.center(3), end='')
+        print(' ')
+
+    # More tedious text formatting: now we format the labels!
+    print('    ' + ('-' * width) + ' ') 
+    longest = len(max(groups.keys(), key=len))
+    # Tricky bit: make every label the same length, then splat so the zip
+    # call groups on a per character basis
+    for item in zip(*[x.ljust(longest) for x in groups.keys()]):
+        print('    ', end='')
+        for letter in item:
+            print(letter.center(3), end='')
+        print(' ')
+
 
 if __name__ == '__main__':
-    # Example code
+    # Example from the exercise page
     food = Category("Food")
     food.deposit(1000, "deposit")
     food.withdraw(10.15, "groceries")
     food.withdraw(15.89, "restaurant and more food for dessert")
     clothing = Category("Clothing")
     food.transfer(50, clothing)
-    print(food)
+
+    # Check the output
+    print(food, '\n')
+    create_spend_chart([food, clothing])
