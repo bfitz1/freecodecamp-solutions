1c5ddfe46074c4beee6d364688e3888601a2ff27
Blair Fitzhugh <bfitz1@umbc.edu>
2024-03-31 23:24:27 -0400

    Flesh out Category class

--- scientific-computing-with-python/budget-app/main.py
+++ scientific-computing-with-python/budget-app/main.py
@@ -1,25 +1,54 @@
 class Category:
-    def __init__(self):
+    def __init__(self, category):
+        self.category = category
         self.ledger = []
     
-    def deposit(self, amount, description=None):
-        pass
+    def deposit(self, amount, description=''):
+        self.ledger.append({'amount': amount, 'description': description})
     
-    def withdraw(self, amount, description=None):
-        pass
+    def withdraw(self, amount, description=''):
+        if not self.check_funds(amount):
+            return False
+        
+        self.ledger.append({'amount': -amount, 'description': description})
+        return True
     
     def get_balance(self):
-        pass
+        return sum(x['amount'] for x in self.ledger)
     
     def transfer(self, amount, category):
-        pass
+        if not self.check_funds(amount):
+            return False
+    
+        category.deposit(amount, f'Transfer from {self.category}')
+        self.withdraw(amount, f'Transfer to {category.category}')
+        return True
     
     def check_funds(self, amount):
-        pass
+        return amount <= self.get_balance()
     
     def __str__(self):
-        pass
+        lines = []
+        lines.append(self.category.center(30, '*'))
+        total = 0
+        for item in self.ledger:
+            line = item['description'][:23].ljust(23, ' ')
+            line += str(item['amount']).rjust(7, ' ')
+            total += item['amount']
+            lines.append(line)
+        lines.append(f'Total: {total:.2f}')
+        return '\n'.join(lines)
 
 
 def create_spend_chart(categories):
     pass
+
+if __name__ == '__main__':
+    # Example code
+    food = Category("Food")
+    food.deposit(1000, "deposit")
+    food.withdraw(10.15, "groceries")
+    food.withdraw(15.89, "restaurant and more food for dessert")
+    clothing = Category("Clothing")
+    food.transfer(50, clothing)
+    print(food)
